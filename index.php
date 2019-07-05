b53d9a702991497e2c685dcf7d5ea7e371303894
Soapheart <soapheart700707@gmail.com>
2019-07-05 11:41:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Specaliance</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+        <script src="js/scroll.js"></script>
+		<script src="https://www.google.com/recaptcha/api.js?render=6LfLHasUAAAAACkEsmiNttVj-gCCRQ0vrJVtuOc0"></script>
+        <!-- fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab" rel="stylesheet">
+        <!-- /fonts -->
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	    (function (d, w, c) {
+		(w[c] = w[c] || []).push(function() {
+		    try {
+		        w.yaCounter49842451 = new Ya.Metrika2({
+		            id:49842451,
+		            clickmap:true,
+		            trackLinks:true,
+		            accurateTrackBounce:true,
+		            webvisor:true
+		        });
+		    } catch(e) { }
+		});
+
+		var n = d.getElementsByTagName("script")[0],
+		    s = d.createElement("script"),
+		    f = function () { n.parentNode.insertBefore(s, n); };
+		s.type = "text/javascript";
+		s.async = true;
+		s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+		if (w.opera == "[object Opera]") {
+		    d.addEventListener("DOMContentLoaded", f, false);
+		} else { f(); }
+	    })(document, window, "yandex_metrika_callbacks2");
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/49842451" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+		<script src="https://www.google.com/recaptcha/api.js?render=reCAPTCHA_site_key"></script>
+    </head>
+    <body>
+        <div class="header">
+            <center><div class="center hLabels">
+                <h1 class="display-4">Specaliance</h1>
+                <p class="lead">Бетон в Краснодаре</p>
+                <p>Круглосуточно и без выходных</p>
+                <img src="img/logo.JPG">
+            </div></center>
+        </div>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapse_target">
+                
+                <a class="navbar-brand"><img src="img/logo.JPG"></a>
+                <ul class="navbar-nav mr-auto" id=#top-menu>
+                    <li class="nav-item hvr-underline-reveal">
+                        <a class="nav-link" href="#f01">Мы предлагаем</a>
+                    </li>
+                    <li class="nav-item hvr-underline-reveal">
+                        <a class="nav-link" href="#f02">Наши преимущества</a>
+                    </li>
+                    <li class="nav-item hvr-underline-reveal">
+                        <a class="nav-link" href="#f03">Расценки</a>
+                    </li>
+                    <li class="nav-item hvr-underline-reveal">
+                        <a class="nav-link" href="#f04">Контакты</a>
+                    </li>
+
+                        <a class="nav-link" href="#"><i class="fab fa-youtube fa-lg"></i></a>
+                        <a class="nav-link" href="https://www.instagram.com/nasos25_52m"><i class="fab fa-instagram fa-lg"></i></a>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <a href="tel:89184700505"><span class="navbar-text phone">
+                        <i class="fas fa-phone fa-lg"></i>
+                    <b> 8 (918) 470-05-05</b></span></a></ul>
+            </div>
+        </nav>
+<!--offer-->
+        <div class="container-fluid offer" id="f01">
+            <div class="container offerContent">
+                <h1>Мы предлагаем</h1>
+                <p>Основными направлениями деятельности ООО "Спецальянс" являются:</p>
+                <div class="row">
+                  <div class="col-12 col-sm-12 col-md-6 off1 offHov">
+                      <div class="center">
+                          <p class="offLabel"><b>Производство и поставка бетонных смесей и растворов любых марок</b></p>
+                      </div>
+                  </div>
+                  <div class="col-12 col-sm-12 col-md-6 off2 offHov">
+                    <div class="center">
+                        <p class="offLabel"><b>Изготовление железобетонных изделий</b></p>
+                    </div>
+                  </div>
+                  <div class="w-100"></div>
+                  <div class="col-12 col-sm-12 col-md-6 off3 offHov">
+                          <div class="center">
+                              <p class="offLabel"><b>Оказание услуг спецтехники</b></p>
+                          </div>
+                      </div>
+                  <div class="col-12 col-sm-12 col-md-6 off4 offHov">
+                      <div class="center">
+                          <p class="offLabel"><b>Общестроительные работы</b></p>
+                      </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+<!--/offer-->
+        <!--request-->
+        <div class="container-fluid request">
+            <div class="container">
+                <div class="row">
+                    <p class="center">Закажите у нас звонок и наши менеджеры в кратчайшие сроки свяжутся с Вами</p>
+                </div>
+                <div class="row">
+                    <button type="button" class="btn btn-warning center" data-toggle="modal" data-target="#callRequest">Заказать звонок</button>
+                </div>
+            </div>
+        </div>
+<!--/request-->
+<!--advantages-->
+        <div class="container-fluid advantages" style="background-color:blanchedalmond" id="f02">
+            <div class="container">
+                <div class="row">
+                    <div class="col-10">
+                    <h1>Наши преимущества</h1>
+                    <p>Наша фирма ООО «СПЕЦАЛЬЯНС» располагает собственной производственной базой, оборудованной бетоносмесительной установкой СБ-145-3A.<br>Основными нашими преимуществами являются:<p>
+                        <ul>
+                            <li><p>выгодное географическое расположение <dfn data-info="Осуществим поставку продукции по кратчайшему пути, в любую точку г. Краснодара"><i class="fas fa-question-circle"></i></dfn></p></li>
+                            <li><p>система автоматического управления производственными процессами <dfn data-info="На базе тензометрических датчиков и контроллеров Direct Logic, обеспечивающая точность дозирования материалов до 1,0 кг"><i class="fas fa-question-circle"></i></dfn></p></li>
+                            <li><p>зимнее исполнение оборудования <dfn data-info="Позволяющее по своим технико-эксплуатационным характеристикам обеспечить устойчивое производство бетонных смесей и строительных растворов при температуре окружающего воздуха от -15°С до +45°С"><i class="fas fa-question-circle"></i></dfn></p></li>
+                            <li><p>автомобильные весы с пределом измерения 80 тн <dfn data-info="Обеспечивающие выходной контроль по объему отгружаемой продукции"><i class="fas fa-question-circle"></i></dfn></p></li>
+                            <li><p>наличие собственного парка автобетоносмесителей <dfn data-info="Позволяющее обеспечить бесперебойную круглосуточную поставку бетонных смесей и строительных растворов на Ваши объекты строительства"><i class="fas fa-question-circle"></i></dfn></p></li>
+                            <li><p>наличие собственного парка автобетононасосов с высотой подачи от 25 до 58 метров <dfn data-info="Для обеспечения подачи бетонных смесей непосредственно в конструкции"><i class="fas fa-question-circle"></i></dfn></li>
+                            
+                            <li><p>собственная ремонтная база, оборудованная боксами для оперативного технического обслуживания, текущих и капитальных ремонтов строительной техники;</p></li>
+                            <li><p>высококвалифицированный персонал с огромным опытом работы в этой области;</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!--/advantages-->
+<!--prices-->
+        <div class="container-fluid priceBlock" id="f03">
+            <div class="container priceTable" style="overflow: auto;">
+            <h1>Расценки на оказание услуг автобетононасоса</h1>
+                <table style="width: 100%; text-align: center;" border="1">
+                    <tbody>
+                        <tr>
+                            <td rowspan="2">Длина стрелы бетононасоса</td>
+                            <td colspan="3">Стоимость услуг бетононасоса</td>
+                        </tr>
+                        <tr>
+                            <td><b>минимальная стоимость</b> выезда</td>
+                            <td><b>при объеме более указанного в пункте "минимальная стоимость"</b>, за каждый м3 перекачанного бетона, к минимальной стоимости выезда автобетононасоса прибавляется, руб/м3</td>
+                            <td><b>при времени работы автобетононасоса более 2-х часов в темпе перекачивания менее 10 м3/час</b>, за каждый час работы автобетононасоса к минимальной стоимости выезда прибавляется, руб/час</td>
+                        </tr>
+                        <tr>
+                            <td>25 м</td>
+                            <td>7000 руб. (2 часа) 24 м3</td>
+                            <td>250 =</td>
+                            <td>2500 =</td>
+                        </tr>
+						<tr>
+							<td>30-32 м</td>
+							<td>8000 руб. (3часа) 28 м3</td>
+							<td>250 =</td>
+							<td>2500 =</td>
+						</tr>
+                        <tr>
+                            <td>36 м</td>
+                            <td>9000 руб. (2 часа) 32 м3</td>
+                            <td>250 =</td>
+                            <td>2500 =</td>
+                        </tr>
+                        <tr>
+                            <td>40-42 м</td>
+                            <td>10000 руб. (2 часа) 36 м3</td>
+                            <td>250 =</td>
+                            <td>2500 =</td>
+                        </tr>
+                        <tr>
+                            <td>52-58 м</td>
+                            <td>15000 руб. (2 часа) 42 м3</td>
+                            <td>350 =</td>
+                            <td>3500 =</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Переезд на объекте, 1000 руб.</td>
+                            <td>При темпе перекачивания менее 10 м3/час <b>автоматически</b> применяется <b>повременная</b> система расчётов</td>
+                            <td>Минимальная смена - 3 часа. Кроме 25 метров. Время технологической промывки (45 минут) входит в оплачиваемое время</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td colspan="3">Дополнительный бетоноводный тракт оплачивается отдельно<br>300 руб. за каждый метр удлинения</td>
+                        </tr>
+                        <tr>
+                            <td colspan="4">При выполнении работ на объектах, расположенных за пределами г.Краснодара, дополнительно применяется расцена за доставку автобетононасоса - 100 руб/км (по удалённости объекта от границы города Краснодара)</td>
+                        </tr>
+                    </tbody>
+                </table>
+            <br>
+            <div class="row">
+               <a href="doc/priceList.docx" class="center"><button type="button" class="btn btn-primary" >Скачать прайс на услуги бетононасоса и бетон в формате docx (MS Word)</button></a>
+            </div>
+        </div>
+        </div>
+<!--/prices-->
+        <!--services-->
+        <div class="container-fluid servicesBlock">
+            <div class="container services">
+                <h1>Наши услуги</h1>
+                <p>Для решения Ваших задач, предлагаем Вам следующие услуги:</p>
+                <ul>
+                    <li><p>круглосуточная поставка товарного бетона и раствора любых марок;</p></li>
+                    <li><p>перевозка бетона (раствора) автобетоносмесителями с объемом смесительного барабана до 10 куб.м;</p></li>
+                    <li><p>подача бетона (раствора) автобетононасосами с длиной стрелы до 42м – 250 руб/куб.м.;</p></li>
+                    <li><p>подача бетона (раствора) автобетононасосами с длиной стрелы до 42м – 250 руб/куб.м.;</p></li>
+                    <li><p>подача бетона (раствора) автобетононасосами с длиной стрелы до58м – 300 руб/куб.м.;</p></li>
+                    <li><p>подача бетона (раствора) автобетононасосами с длиной стрелы 58м – 350 руб/куб.м.;</p></li>
+                    <li><p>аренда стационарных распределительных стрел от 150 000 руб/месяц;</p></li>
+                    <li><p>аренда стационарного бетононасоса от 300 000 руб/месяц.</p></li>
+                </ul>
+            </div>
+        </div>
+<!--/services-->
+<!--request-->
+        <div class="container-fluid request">
+            <div class="container">
+                <div class="row">
+                    <p class="center">Закажите у нас звонок и наши менеджеры в кратчайшие сроки свяжутся с Вами</p>
+                </div>
+                <div class="row">
+                    <button type="button" class="btn btn-warning center" data-toggle="modal" data-target="#callRequest">Заказать звонок</button>
+                </div>
+            </div>
+        </div>
+<!--/request-->
+<!--friends-->
+        <div class="container-fluid clientsBlock">
+            <div class="container clients">
+                <h1>Наши клиенты</h1>
+                <p>Общество с ограниченной ответственностью "Спецальянс" работает на строительном рынке Краснодарского края с 2000 г.<br>Среди наших заказчиков такие организации, как ЗАО «Таманьнефтегаз», ООО НПФ «Металлимпресс», ООО «Девелопмент-Юг» и др. За годы работы наша компания приняла участие в строительстве и реконструкции большого количества объектов на территории Краснодарского края и за его пределами. В их числе промышленные объекты: Туапсинский, Афипский, Краснодарский нефтеперерабатывающие заводы, Абинский электрометаллургический завод, консервныйперерабатывающий завод «Бондюэль-Кубань», торговые и развлекательные комплексы «Мега Адыгея», «СБС», «OZ», «Галерея», «Лента», завод ”KLAAS” а также большое число жилых и общественно-деловых комплексов.</p>
+            </div>
+        </div>
+<!--/friends-->
+<!--contacts-->
+        <div class="container-fluid contacts" id="f04">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A046b363a742e5eac132f31aad2487bde8277d8376a2a89eee03ee38d1c036592&amp;source=constructor" width="100%" height="320" frameborder="0"></iframe>
+                    </div>
+                    <div class="col-12 col-md-6 contactsLables">
+                        <h1>Контакты</h1>
+						Адрес:<br>г.Краснодар, пер.Звёздный, 1/1<br><br><a href="tel:89184700505">Тел.: 8 (918) 470-05-05</a><br><a href="tel:89183564849">Тел.: 8 (918) 356-48-49</a><br><br>
+                        <a href="mailto:zakaz.nasos@mail.ru">zakaz.nasos@mail.ru</a>
+                </div>
+            </div>
+            <div class="row"></div>
+                <span>Дизайн и разработка - <a href="https://vk.com/soapheart">Soapheart</a>, 2018</span>
+            </div>
+        </div>
+<!--/contacts-->
+<!--modal-->
+<div class="modal fade" id="callRequest" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+    <form action="php/send.php" method="post">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Заказать звонок</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Пожалуйста заполните поля ниже, чтобы мы могли связаться с Вами</p>
+        <input type="text" name="name" placeholder="Ваше имя" required="required" /><br/>
+        <input type="phone" name="phone" placeholder="Телефон" required="required"/><br/>
+        <input type="email" name="email" placeholder="Адрес@почты.ru"/><br/>
+		<input type="text" name="address" class="hidden" placeholder="Адрес"/><br/>
+        <div id="result"></div>
+      </div>
+      <div class="modal-footer">
+		  <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+          <button type="submit" class="btn btn-primary center">Заказать звонок</button>
+      </div>
+    </form>
+	<script>
+		grecaptcha.ready(function() {
+		  grecaptcha.execute('6LfLHasUAAAAACkEsmiNttVj-gCCRQ0vrJVtuOc0', {action: 'homepage'}).then(function(token) {
+			 document.getElementById('g-recaptcha-response').value=token;
+		  	});
+	  	});
+  	</script>
+    </div>
+  </div>
+</div>
+<!--/modal-->
+</body>
--- php/send.php
+++ php/send.php
@@ -1,23 +1,43 @@
 <?
 header('Content-Type: text/html; charset=utf-8');
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){
-        $to = 'zakaz.nasos@mail.ru';
-        $subject = 'Форма на сайте';
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>
-                        <p>Почта: '.$_POST['email'].'</p>
-                    </body>
-                </html>';
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-        $headers .= "From: Отправитель <from@example.com>\r\n";
-        mail($to, $subject, $message, $headers);
-        echo "Ваш запрос успешно отправлен, спасибо Вам! Мы скоро свяжемся с Вами.";
-        echo "<br /><br /><a href ='http://xn--80abm6aaebc0abd.xn--p1ai/'>Вернуться на сайт</a>";
-}
+
+define('SITE_KEY', '6LfLHasUAAAAACkEsmiNttVj-gCCRQ0vrJVtuOc0');
+define('SECRET_KEY', '6LfLHasUAAAAAIizOxlMIGc8WJ1b_RuWNhPUBV9U');
+
+if($_POST){
+	function getCaptcha($SecretKey) {
+		$Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".SECRET_KEY."&response={$SecretKey}"); 
+		$Return = json_decode($Response);
+		return $Return;
+		}
+	
+	$Return = getCaptcha($_POST['g-recaptcha-response']);
+	
+	if($Return->success == true && $Return->score >= 0.5){
+			$to = 'zakaz.nasos@mail.ru';
+			$subject = 'Форма на сайте';
+			$message = '
+					<html>
+						<head>
+							<title>'.$subject.'</title>
+						</head>
+						<body>
+							<p>Имя: '.$_POST['name'].'</p>
+							<p>Телефон: '.$_POST['phone'].'</p>
+							<p>Почта: '.$_POST['email'].'</p>
+						</body>
+					</html>';
+			$headers  = "Content-type: text/html; charset=utf-8 \r\n";
+			$headers .= "From: Бетононасос.рф <from@example.com>\r\n";
+			mail($to, $subject, $message, $headers);
+			echo "Ваш запрос успешно отправлен, спасибо Вам! Мы скоро свяжемся с Вами.";
+			echo "<br /><br /><a href ='http://xn--80abm6aaebc0abd.xn--p1ai/'>Вернуться на сайт</a>";
+		}
+		else {
+			echo "Вы точно человек?";
+		}
+	}
+	else{
+		echo "Условие не фурычит";
+	}
 ?>
